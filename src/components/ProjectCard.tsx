c8a13a8b213f967c33f0344bc572a762ca70b794
Victor Castro <victor.castro34@gmail.com>
2025-07-07 14:34:39 -0300

    removed placeholder project content

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,20 +1,21 @@
+import { Project } from '@/types/project';
 import Image from 'next/image';
 
-const ProjectCard = () => {
+const ProjectCard = ({ project }: { project: Project }) => {
   return (
     <div className="border border-[var(--border)] rounded-xl shadow-lg p-4 md:p-6 max-w-xs w-full bg-[var(--background-secondary)] flex flex-col items-center gap-2 md:gap-4 hover:bg-[var(--hover-bg)] transition-colors duration-200 theme-transition">
       <div className="w-full h-28 md:h-40 overflow-hidden rounded-lg flex items-center justify-center bg-[var(--border)]">
         <Image
-          src="/globe.svg"
-          alt="Project Preview"
+          src={project.image_path}
+          alt={project.title}
           width={320}
           height={160}
           className="object-cover w-full h-full"
         />
       </div>
-      <h3 className="text-lg md:text-xl font-semibold mt-2 mb-1 text-[var(--text-primary)]">Project Title</h3>
+      <h3 className="text-lg md:text-xl font-semibold mt-2 mb-1 text-[var(--text-primary)]">{project.title}</h3>
       <p className="text-[13px] md:text-base text-[var(--text-secondary)] text-center m-0">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc.
+        {project.description}
       </p>
       <div className="flex gap-1 md:gap-2 flex-wrap justify-center">
         <span className="bg-[var(--border)] text-[var(--text-secondary)] rounded-md px-2 md:px-3 py-1 text-xs md:text-sm border border-[var(--border)]">React</span>
--- src/views/ProjectsPage.tsx
+++ src/views/ProjectsPage.tsx
@@ -1,13 +1,21 @@
+'use client'
 import ProjectCard from '../components/ProjectCard';
+import { UserContext } from '@/contexts/UserContext';
+import { useContext } from 'react';
 
 const ProjectsPage = () => {
+  const { projects } = useContext(UserContext)
+
+
   return (
     <div className="p-4 md:p-6 bg-[var(--background)] text-[var(--text-primary)] theme-transition">
       <h1 className="text-xl md:text-2xl font-bold mb-4 md:mb-6">Projects</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {projects?.map((project) => {
+          return (
+            <ProjectCard key={project.id} project={project} />
+          )
+        })}
       </div>
     </div>
   );
