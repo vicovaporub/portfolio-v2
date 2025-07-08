b8c3cdbba55172aa1275e7c0ebc1627ce4d3ab45
Victor Castro <victor.castro34@gmail.com>
2025-07-08 16:58:40 -0300

    added texts

--- src/lib/base.ts
+++ src/lib/base.ts
@@ -41,4 +41,42 @@ export function validateLocalizedTextFormat(text: string): boolean {
   return true;
 }
 
-
+export const portfolioTexts = {
+  pages: {
+    welcome: {
+      title: 'Portfolio page ^^pt-BR:Página de Portfólio',
+      mostUsedSkills: 'Most used skills ^^pt-BR:Habilidades mais usadas',
+    },
+    about: {
+      title: 'About ^^pt-BR:Sobre',
+    },
+    projects: {
+      title: 'Projects ^^pt-BR:Projetos',
+      project: {
+        title: 'Project ^^pt-BR:Projeto',
+        description: 'Project description ^^pt-BR:Descrição do projeto',
+        technologies: 'Technologies ^^pt-BR:Tecnologias',
+        link: 'Link ^^pt-BR:Link'
+      }
+    },
+    skills: {
+      title: 'Skills ^^pt-BR:Habilidades',
+      skill: {
+        title: 'Skill ^^pt-BR:Habilidade',
+        description: 'Skill description ^^pt-BR:Descrição da habilidade',
+      }
+    },
+    contact: {
+      title: 'Contact ^^pt-BR:Contato',
+    }
+  },
+  menu: {
+    menuLabel: 'Menu ^^pt-BR:Menu',
+    menuItem: {
+      about: 'About ^^pt-BR:Sobre',
+      projects: 'Projects ^^pt-BR:Projetos',
+      skills: 'Skills ^^pt-BR:Habilidades',
+      contact: 'Contact ^^pt-BR:Contato'
+    }
+  }
+}
