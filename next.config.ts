bc2c00dc088736b763bf5c86044fe681e8772b0a
Victor Castro <victor.castro34@gmail.com>
2025-12-05 19:04:22 -0300

    fixed next image

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,23 @@
 import type { NextConfig } from "next";
 
+const supabaseImgHost = process.env.SUPABASE_IMG_PATH
+    ? new URL(process.env.SUPABASE_IMG_PATH).hostname
+    : undefined;
+
 const nextConfig: NextConfig = {
     devIndicators: {
         position: "bottom-right",
     },
     images: {
-        remotePatterns: [{ hostname: process.env.SUPABASE_IMG_PATH || "" }],
+        remotePatterns: supabaseImgHost
+            ? [
+                  {
+                      protocol: "https",
+                      hostname: supabaseImgHost,
+                      pathname: "/**",
+                  },
+              ]
+            : [],
     },
 };
 
