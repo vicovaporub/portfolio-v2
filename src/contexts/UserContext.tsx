7a666aca69fb8c77304b62f1eddef9df0886abeb
Victor Castro <victor.castro34@gmail.com>
2026-01-05 17:59:34 -0300

    added technologies to context

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,5 +1,6 @@
 import { Project } from "@/types/project";
 import { User } from "@/types/user";
+import { Technology } from "@/types/technology";
 import { createContext, useState } from "react";
 
 export interface AboutData {
@@ -11,6 +12,7 @@ interface UserContextType {
     user?: User;
     projects?: Project[];
     about?: AboutData;
+    technologies?: Technology[];
     isLoading: boolean;
     error: string | null;
 }
@@ -19,6 +21,7 @@ export const UserContext = createContext<UserContextType>({
     user: undefined, 
     projects: undefined,
     about: undefined,
+    technologies: undefined,
     isLoading: true,
     error: null
 });
@@ -28,17 +31,19 @@ interface UserProviderProps {
     initialUser?: User;
     initialProjects?: Project[];
     initialAbout?: AboutData;
+    initialTechnologies?: Technology[];
 }
 
-export const UserProvider = ({ children, initialUser, initialProjects, initialAbout }: UserProviderProps) => {
+export const UserProvider = ({ children, initialUser, initialProjects, initialAbout, initialTechnologies }: UserProviderProps) => {
     const [user] = useState<User | undefined>(initialUser)
     const [projects] = useState<Project[] | undefined>(initialProjects)
     const [about] = useState<AboutData | undefined>(initialAbout)
+    const [technologies] = useState<Technology[] | undefined>(initialTechnologies)
     const [isLoading] = useState(false)
     const [error] = useState<string | null>(null)
     
     return (
-        <UserContext.Provider value={{ user, projects, about, isLoading, error }}>
+        <UserContext.Provider value={{ user, projects, about, technologies, isLoading, error }}>
             {children}
         </UserContext.Provider>
     )
