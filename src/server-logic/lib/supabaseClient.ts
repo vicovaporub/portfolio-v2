a79f8a9ee3ba9ebee104e3c65a605b9785fd5f9c
Victor Castro <victor.castro34@gmail.com>
2026-01-06 23:53:00 -0300

    updated Supabase client to use service role key instead of anon key

--- src/server-logic/lib/supabaseClient.ts
+++ src/server-logic/lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 
 const supabaseUrl = process.env.SUPABASE_URL || ''
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || ''
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey)
